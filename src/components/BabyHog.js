81fa8e61ada3a29a375f8703a1fa00e1487679b4
Silvanos Eric <ericoderito@gmail.com>
2024-07-31 16:16:45 +0300

    feat: add dynamic weight functionality and rounding to BabyHog component
    - Implement useState for dynamic weight management in BabyHog component.
    - Add rounding function to ensure weight is displayed to one decimal place.
    - Update weight display to show weight in kilograms.
    - Refactor weight change handler to adjust weight based on button clicks.

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import sunBaby from "../assets/sun-eyes.png";
 import blueBaby from "../assets/blue-eyes.png";
@@ -17,24 +17,41 @@ function eyeColorMapper(color) {
   return mapper[color];
 }
 
+function roundToOneDP(number) {
+  return Math.round(number * 10) / 10;
+}
+
 function BabyHog({ name, hobby, eyeColor }) {
+  const [weight, setWeight] = useState(
+    Math.round((Math.random() * (2 - 1.1) + 1.1) * 10) / 10
+  );
+
   eyeColor = eyeColor ? eyeColor : "normal";
   const mappedImage = eyeColorMapper(eyeColor);
 
   function handleChangeWeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
+    if (e.target.matches('button[name="+"]')) {
+      setWeight(roundToOneDP(weight + 0.1));
+    } else if (e.target.matches('button[name="-"]')) {
+      setWeight(roundToOneDP(weight - 0.1));
+    }
   }
 
   return (
     <li className="hogbabies">
       <h1>{name}</h1>
-      <h3>Weight:</h3>
+      <h3>Weight: {weight}kg</h3>
       <h3>Hobby: {hobby}</h3>
       <h4>Eye Color: {eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button name="+" onClick={handleChangeWeight}>
+        Increase Weight
+      </button>
+      <button name="-" onClick={handleChangeWeight}>
+        Decrease Weight
+      </button>
 
       <div className="hb-wrap">
         <img
